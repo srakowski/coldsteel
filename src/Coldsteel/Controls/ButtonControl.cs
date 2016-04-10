106df175bce35d3649195ff31353108c7cd31baa
Shawn Rakowski <srakowski@live.com>
2016-04-09 21:04:40 -0500

    add keyboard button control classes

---
+++ src/Coldsteel/Controls/ButtonControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coldsteel.Controls
+{
+    public abstract class ButtonControl : Control
+    {
+        public ButtonControl(string key) : base(key)
+        {
+        }
+    }
+}
---
+++ src/Coldsteel/Controls/KeyboardButtonControl.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coldsteel.Controls
+{
+    public class KeyboardButtonControl : ButtonControl
+    {
+        public KeyboardButtonControl(string controlKey, Keys keyboardKey) : base(controlKey)
+        {
+        }
+    }
+}
