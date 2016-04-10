a998ec8bcf6904ac20828c4b294de872302e6f19
Shawn Rakowski <srakowski@live.com>
2016-04-09 21:55:44 -0500

    stub SpriteRenderer

---
+++ src/Coldsteel/Renderers/SpriteRenderer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coldsteel.Renderers
+{
+    public class SpriteRenderer : Renderer
+    {
+        public SpriteRenderer(Layer layer, Texture2D texture)
+        {
+        }
+
+        public override void Render(IGameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
