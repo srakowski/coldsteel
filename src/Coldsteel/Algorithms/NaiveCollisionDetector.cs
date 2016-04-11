c13f1136d5f46dad06155c7f801583e9d9c29f24
Shawn Rakowski <srakowski@live.com>
2016-04-10 19:48:50 -0500

    naive collision detection implemented

--- src/Coldsteel/Algorithms/NaiveCollisionDetector.cs
+++ src/Coldsteel/Algorithms/NaiveCollisionDetector.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,6 +9,18 @@ namespace Coldsteel.Algorithms
     {
         public void DetectCollisions(IEnumerable<Collider> colliders, Action<Collider, Collider> onCollision)
         {
+            for (var i = 0; i < colliders.Count() - 1; i++)
+                for (var j = i + 1; j < colliders.Count(); j++)
+                {
+                    var collider1 = colliders.ElementAt(i);
+                    var bounds1 = collider1.Bounds;
+
+                    var collider2 = colliders.ElementAt(j);
+                    var bounds2 = collider2.Bounds;
+
+                    if (bounds1.Intersects(bounds2))
+                        onCollision(collider1, collider2);
+                }
         }
     }
 }
--- src/Coldsteel/Collider.cs
+++ src/Coldsteel/Collider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,8 @@ namespace Coldsteel
 {
     public abstract class Collider : GameObjectComponent
     {
+        internal abstract Rectangle Bounds { get; }
+
         internal void NotifyCollision(Collider collider)
         {
             this.GameObject?.NotifyCollision(new Collision(collider.GameObject));
