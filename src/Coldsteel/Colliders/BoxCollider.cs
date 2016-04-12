02e625bba0a119ae33c8ac2258c4b89f1e37816b
Shawn Rakowski <srakowski@live.com>
2016-04-12 12:56:53 -0500

    apparently BoxCollider wasn't checked in

---
+++ src/Coldsteel/Colliders/BoxCollider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coldsteel.Colliders
+{
+    public class BoxCollider : Collider
+    {
+        internal override Rectangle Bounds
+        {
+            get
+            {
+                var t = this.GameObject.GetComponent<Transform>();
+                var pos = t.Position;
+                return new Rectangle((int)(pos.X - (_width / 2)), (int)(pos.Y - (_height / 2)),
+                    (int)_width, (int)_height);
+            }
+        }
+
+        private int _width = 0;
+
+        private int _height = 0;
+
+        public BoxCollider(int width, int height)
+        {
+            this._width = width;
+            this._height = height;
+        }
+    }
+}
