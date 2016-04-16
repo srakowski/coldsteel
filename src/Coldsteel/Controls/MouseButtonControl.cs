698ab57e28acc610c231b29daac72bd5a625b822
Shawn Rakowski <srakowski@live.com>
2016-04-16 08:11:33 -0500

    mouse button input

---
+++ src/Coldsteel/Controls/MouseButtonControl.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coldsteel.Controls
+{
+    public enum MouseButton
+    {
+        Left,
+        Right,
+        Middle
+    }
+
+    public class MouseButtonControl : ButtonControl
+    {
+        public override bool IsDown()
+        {
+            return _mouseButton == MouseButton.Left ? InputDevices.MouseState.LeftButton == ButtonState.Pressed :
+                _mouseButton == MouseButton.Right ? InputDevices.MouseState.RightButton == ButtonState.Pressed :
+                _mouseButton == MouseButton.Middle ? InputDevices.MouseState.MiddleButton == ButtonState.Pressed :
+                false;
+        }
+
+        private MouseButton _mouseButton;
+
+        public MouseButtonControl(MouseButton mouseButton)
+        {
+            _mouseButton = mouseButton;
+        }
+    }
+}
