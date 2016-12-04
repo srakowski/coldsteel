734689d93b472d776fd1efdc0807ab632837487d
Shawn Rakowski <srakowski@live.com>
2016-12-04 12:52:41 -0600

    start coldsteel studio

---
+++ src/Coldsteel.Studio/Modules/Startup/Module.cs
@@ -0,0 +1,14 @@
+﻿using Gemini.Framework;
+using System.ComponentModel.Composition;
+
+namespace Coldsteel.Studio.Modules.Startup
+{
+    [Export(typeof(IModule))]
+    public class Module : ModuleBase
+    {
+        public override void Initialize()
+        {
+            MainWindow.Title = "Coldsteel";
+        }
+    }
+}
