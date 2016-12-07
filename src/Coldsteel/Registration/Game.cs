98ebb79ceb4c1495d93197c2b0b0c7e54561648d
srakowski <srakowski@live.com>
2016-12-06 19:19:49 -0600

    begin registration namespace with Game

---
+++ src/Coldsteel/Registration/Game.cs
@@ -0,0 +1,24 @@
+﻿// MIT License - Copyright (C) Shawn Rakowski
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+namespace Coldsteel.Registration
+{
+    public class Game
+    {
+        /// <summary>
+        /// The title of this Game.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Assembly references required for this Game.
+        /// </summary>
+        public string[] References { get; set; }
+
+        /// <summary>
+        /// The name of the Scene that should be loaded when the Game is Loaded.
+        /// </summary>
+        public string StartupScene { get; set; }
+    }
+}
