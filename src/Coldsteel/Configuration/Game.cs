07115139ff5c041c76a0deb4a7f544c7939876c5
srakowski <srakowski@live.com>
2016-12-06 19:24:57 -0600

    make registration configuration instead

--- src/Coldsteel/Configuration/Game.cs
+++ src/Coldsteel/Configuration/Game.cs
@@ -2,7 +2,7 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 
-namespace Coldsteel.Registration
+namespace Coldsteel.Configuration
 {
     public class Game
     {
