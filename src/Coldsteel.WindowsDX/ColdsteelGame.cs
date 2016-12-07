b30eeb122020578d4f838c915b430bbbdf47b51f
srakowski <srakowski@live.com>
2016-12-06 19:51:20 -0600

    create bootstrapper

--- src/Coldsteel.WindowsDX/ColdsteelGame.cs
+++ src/Coldsteel.WindowsDX/ColdsteelGame.cs
@@ -14,7 +14,7 @@ namespace Coldsteel
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            Engine.Attach(this);
+            Components.Add(new Bootstrapper(this));
         }
     }
 }
---
+++ src/Coldsteel/Bootstrapper.cs
@@ -0,0 +1,54 @@
+﻿// MIT License - Copyright (C) Shawn Rakowski
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using Microsoft.Xna.Framework;
+using System.Collections;
+using System.Reflection;
+
+namespace Coldsteel
+{
+    /// <summary>
+    /// Bootstraps the Game based on the "game.xml" Configuration.Game content.
+    /// </summary>
+    public class Bootstrapper : GameComponent
+    {
+        private SceneManager _sceneManager;
+
+        private IEnumerator _tasks;
+
+        public Bootstrapper(Game game) : base(game)
+        {
+            _sceneManager = new SceneManager(game);
+            game.Components.Add(_sceneManager);
+            game.Components.Add(new InputManager(game));
+            game.Components.Add(new ScriptingManager(game));
+            game.Components.Add(new RenderingManager(game));
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            var gameConfiguration = Game.Content.Load<Configuration.Game>("game");
+            _tasks = Begin(gameConfiguration);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (!_tasks.MoveNext())
+                Game.Components.Remove(this);
+        }
+
+        private IEnumerator Begin(Configuration.Game gameConfiguration)
+        {
+            foreach (var reference in gameConfiguration.References)
+            {
+                yield return $"loading assembly {reference}";
+                Assembly.LoadFrom(reference);
+            }
+
+            yield return $"starting scene {gameConfiguration.StartupScene}";
+            _sceneManager.Start(gameConfiguration.StartupScene);
+        }
+    }
+}
